1cf638d6b8d55d3705744dd0f60c237825971ec0
Saim  Ibrahim <saimibrahim@saims-mbp.lan>
2023-06-26 22:28:56 -0400

    Resolve merge conflict.

--- debug.sh
+++ debug.sh
@@ -3,19 +3,11 @@ set -eu -o pipefail -E
 
 echo "=============================================="
 
-<<<<<<< HEAD
-screencapture test.png
-echo "Uploading screenshot..."
-if ! curl -w "\n" --upload-file test.png "https://transfer.sh/$(openssl rand -hex 8).png"; then
-    echo "Error uploading screenshot"
-fi
-=======
 screencapture test.jpg
 echo "Uploading screenshot..."
 until curl https://bashupload.com -T test.jpg; do
     echo "Error, sleeping"
     sleep 1
 done
->>>>>>> e9b6a95 (Initial commit)
 
 echo "=============================================="
